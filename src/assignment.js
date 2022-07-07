2e35d39552db269012d897ddeca5622ddf7efde0
OduboEbimobereFavour <108741441+OduboEbimobereFavour@users.noreply.github.com>
2022-07-07 20:28:28 +0000

    Check challenge

--- src/assignment.js
+++ src/assignment.js
@@ -8,15 +8,17 @@ const assignment = {};
  * Challenge - 1
  * 
  * Calculate and return the sum of the numbers in an array.
+        
  * If you did Challenge - 1, remove the comment in the line just after this function
  * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+    return arrayOfNumbers.reduce((accumulator, number) => accumulator += number, 0)
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +30,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        if ((i != 0) && i % 2 === 0) {
+            count++
+        }
+    }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
